c82558894024c7cdb4bc4aacfe06a0ea2b766c3d
Manal essalmouni <115215370+manalessalmouni@users.noreply.github.com>
2025-06-04 23:11:14 +0100

    Create waypoints.py

---
+++ src/waypoints_pkg/waypoints_pkg/waypoints.py
@@ -0,0 +1,14 @@
+# waypoints_pkg/waypoints.py
+
+waypoints = [
+    (1.0, 0.0),
+    (2.0, 1.0),
+    (1.0, 2.0),
+    (0.0, 1.0),
+    (1.0, 0.0),  # boucle 1 terminée
+    (2.0, -1.0),
+    (3.0, 0.0),
+    (2.0, 1.0),
+    (1.0, 0.0)   # boucle 2 terminée
+]
+
